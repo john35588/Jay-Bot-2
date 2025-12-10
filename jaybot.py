38abf5f6c8a5a0b002fc725ebbadbf25eb86ab95
John Wendel <jcwendel100@gmail.com>
2025-12-10 12:27:27 -0700

    Tweaked persona, changed model to Gemma3

--- jaybot.py
+++ jaybot.py
@@ -8,7 +8,7 @@ intents.message_content = True
 client = discord.Client(intents=intents)
 
 OLLAMA_URL = os.getenv("OLLAMA_URL")
-MODEL = "llama3.2:latest"
+MODEL = "gemma3:latest"
 GENERAL_PERSONA_PATH = "persona/new-persona.txt"
 MINECRAFT_PERSONA_PATH = "persona/minecraft-persona.txt"
 MENTIONED_PERSONA_PATH = "persona/mentioned-persona.txt"
